f527a374f9f0fdd27bc0d6116ada550e9f32c99d
DrEden33773 <edwardwang33773@gmail.com>
2023-07-31 21:48:48 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,52 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
+pub mod exit;
+pub mod serial;
+pub mod test_framework;
 #[cfg(test)]
 pub mod tests;
-
 pub mod vga_buffer;
 
 use core::panic::PanicInfo;
+use test_framework::Testable;
 
-#[cfg(test)]
-pub(crate) fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-}
-
-#[panic_handler]
-pub(crate) fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
+/// Entry / Main
 #[no_mangle]
 pub(crate) extern "C" fn _start() -> ! {
     println!(" -*-*-*- My ROS -*-*-*- ");
     println!();
     #[cfg(test)]
     test_main();
+    loop {}
+}
+
+#[cfg(test)]
+pub(crate) fn test_runner(tests: &[&dyn Testable]) {
+    use crate::exit::{exit_qemu, QemuExitCode};
+    serial_println!();
+    serial_println!("Running {} tests", tests.len());
+    serial_println!();
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+pub(crate) fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    use crate::exit::{exit_qemu, QemuExitCode};
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,4 @@
-use crate::{print, println};
-
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
 }
