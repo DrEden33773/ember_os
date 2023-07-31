61db17192d93b94ec30687106762d1e4b538d217
DrEden33773 <edwardwang33773@gmail.com>
2023-07-31 20:51:35 +0800

    simple structure

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+pub mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+pub(crate) fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+pub(crate) fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+pub(crate) extern "C" fn _start() -> ! {
+    println!("Hello, world!");
     loop {}
 }
