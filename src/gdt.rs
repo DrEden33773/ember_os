d9faf98ad88c726b9e061d18c5602800548c652d
DrEden33773 <edwardwang33773@gmail.com>
2024-01-17 20:09:59 +0800

    use addr_of! instead of & operator

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,3 +1,5 @@
+use core::ptr::addr_of;
+
 use lazy_static::lazy_static;
 use x86_64::{structures::tss::TaskStateSegment, VirtAddr};
 
@@ -9,7 +11,7 @@ lazy_static! {
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
+            let stack_start = VirtAddr::from_ptr(unsafe { addr_of!(STACK) });
             stack_start + STACK_SIZE // stack_end
         };
         tss
