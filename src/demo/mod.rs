6c760fd1af80823d22483847291a037309b37021
DrEden33773 <edwardwang33773@gmail.com>
2024-03-06 21:13:50 +0800

    update

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -13,7 +13,7 @@ pub mod println_eprintln;
 pub fn run_synchronous_demos(boot_info: &'static BootInfo) {
   memory::show_map_of_tables(boot_info);
   println_eprintln::show_color_diff();
-  // heap_allocation::create_box();
-  // heap_allocation::create_vec();
-  // heap_allocation::create_reference_counted_vec();
+  heap_allocation::create_box();
+  heap_allocation::create_vec();
+  heap_allocation::create_reference_counted_vec();
 }
